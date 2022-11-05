86602538a03d37bf7d62de984cef9bd4209c39b9
Bahtina Svetlana <bahtina.sv05@mail.ru>
2022-11-05 20:08:05 +0300

    1

--- src/lesson4/task1/List.kt
+++ src/lesson4/task1/List.kt
@@ -255,6 +255,7 @@ fun convert(n: Int, base: Int): MutableList<Int> {
  * (например, n.toString(base) и подобные), запрещается.
  */
 fun convertToString(n: Int, base: Int): String {
+    if (n == 0) return "0"
     var m = n
     var v = ""
     val k = listOf<String>("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f", "g", "h",
