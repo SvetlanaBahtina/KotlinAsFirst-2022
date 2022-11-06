dca1502494ed0e9e9ed97b1a9f4e75a83a4b5cfe
Bahtina Svetlana <bahtina.sv05@mail.ru>
2022-11-06 19:17:09 +0300

    1

--- src/lesson5/task1/Map.kt
+++ src/lesson5/task1/Map.kt
@@ -242,13 +242,7 @@ fun findCheapestStuff(stuff: Map<String, Pair<String, Double>>, kind: String): S
  * Например:
  *   canBuildFrom(listOf('a', 'b', 'o'), "baobab") -> true
  */
-fun canBuildFrom(chars: List<Char>, word: String): Boolean {
-    val w = word.toLowerCase().toSet()
-    for (i in chars.indices) chars[i].toLowerCase()
-    if (w == chars.toSet()) return true
-    return false
-}
-
+fun canBuildFrom(chars: List<Char>, word: String): Boolean = TODO()
 /**
  * Средняя (4 балла)
  *
